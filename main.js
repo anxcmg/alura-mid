9088642a4a1d9fcc9713d05df4b9fc8df2f78118
anxcmg <138674104+anxcmg@users.noreply.github.com>
2023-10-11 12:41:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function tocasompom(){
+function tocaSomPom(){
      document.querySelector('#som_tecla_pom').play('');   
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom ();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
 
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocaSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;
